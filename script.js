709c69ea2c01fa7fd76ddc9fb1328364223b2113
winsofttutorials <winsofttutorials@gmail.com>
2023-05-12 23:29:21 +0100

    js functionality for menu-icon

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function mouseOvering() {
   }
   event.currentTarget.classList.add("activeLink");
 }
+
+// Small screen nav toggle
+
+let navToggle = document.getElementById("menu-icon");
+let navLinks = document.getElementById("navLinks");
+let mockups = document.getElementById("mockUp");
+let heroSegment = document.getElementById("heroSegment");
+
+function navOpen() {
+  if (navLinks.style.display == "none") {
+    navLinks.style.display = "block";
+    navToggle.src = "./images/icon-close.svg";
+    mockups.style.display = "none";
+    heroSegment.style.backgroundColor = "#00000033";
+  } else {
+    navLinks.style.display = "none";
+    navToggle.src = "./images/icon-hamburger.svg";
+    mockups.style.display = "block";
+    heroSegment.style.backgroundColor = "";
+  }
+}
